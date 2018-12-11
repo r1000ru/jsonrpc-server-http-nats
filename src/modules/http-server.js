3c088a3c8c38a22c806662eae22420a64ddc4f6d
Сорокин Роман Александрович <rsorokin@TEC-ELECTRONICS.RU>
2018-12-11 12:26:11 +0300

    Init

---
+++ src/modules/http-server.js
@@ -0,0 +1,93 @@
+const http = require('http');
+
+var Server = function(server) {
+    this._server = server || http.createServer();
+
+    this._headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS, POST",
+        "Access-Control-Allow-Headers": "Origin, Accept, Content-Type",
+    };
+
+    this._server.on('request', (request, response) => {
+        this._onRequest(request, response);
+    });
+
+    this.onRequest = function(input, callback) {
+        callback(input);
+    }
+}
+
+
+/**
+ * Устанавливает пользовательские заголовки
+ */
+Server.prototype.setHTTPHeaders = function(headers) {
+    this._headers = headers || {};
+}
+
+/**
+ * Включает ожидание входящих подключений
+ */
+Server.prototype.listen = function(options, callback) {
+    if (typeof(options) === 'function') {
+        callback = options;
+        options = {};
+    }
+    
+    options = options || {};
+    options.host = options.host || '127.0.0.1';
+    options.port = options.port || 8080;
+
+    this._server.listen(options, callback);
+}
+
+/**
+ * Метод обарабатывающих входящие запросы
+ */
+Server.prototype._onRequest = function(request, response) {
+    // Обработка OPTIONS
+    if (request.method === 'OPTIONS') {
+        this._send(response);
+        return;
+    }
+
+    // Загружаем тело
+    this._getRequestBody(request, (input)=>{
+        // Вызываем внешний обработчки
+        this.onRequest(input, 'http', (output)=>{
+            // Отправляем ответ
+            this._send(response, output);
+        });
+    })
+}
+
+/**
+ * Метод загружает тело POST запроса
+ */
+Server.prototype._getRequestBody = function(request, callback) {
+    let buffers = [];
+    request.on('data', (chunk) => {
+        buffers.push(chunk);
+    }).on('end', () => {
+        let content = Buffer.concat(buffers).toString('utf8');
+        callback(content);
+    }).on('error', (e)=>{
+        console.log(e);
+        callback();
+    });
+}
+
+/**
+ * Метод отправляет ответ на запрос
+ */
+Server.prototype._send = function(response, content) {
+    let headers = Object.assign({}, this._headers, {
+        "Content-Type": "application/json",
+        "Content-Length": Buffer.from(content).byteLength
+    })
+    response.writeHead(200, headers);
+    response.end(content);
+}
+
+module.exports = Server;
---
+++ src/modules/nats-server.js
@@ -0,0 +1,50 @@
+const NATS = require('nats');
+
+var NatsServer = function() {
+    this._server;
+    
+    this.onRequest = function(input, requestChannel, callback) {
+        callback('{}');
+    }
+}
+
+NatsServer.prototype._onRequest = function(input, requestChannel, responseChannel) {
+    this.onRequest(input, requestChannel, (output)=>{
+        this._server.publish(responseChannel, output || {});
+    });
+}
+
+NatsServer.prototype.addChannel = function(channel, callback) {
+    if (!this._server) {
+        if (callback) {
+            callback('NATS is not connected.')
+        }
+        return;
+    }
+    
+    this._server.subscribe(channel, (input, responseChannel, requestChannel)=>{
+        this._onRequest(input, requestChannel, responseChannel);
+    });
+
+    if (callback) {
+        callback();
+    }
+
+}
+
+NatsServer.prototype.listen = function(options, channel, callback) {
+    if (typeof(options) === 'string') {
+        options = {
+            url: options
+        }
+    }
+    
+    this._server = NATS.connect(options);
+
+    this._server.on('connect', ()=>{
+        this.addChannel(channel, callback)
+    });
+    
+}
+
+module.exports = NatsServer;
